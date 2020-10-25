7fde8e1899f332dd445bd4e98e83628608a06b51
Abdul Hamid HmDi <abdelhamid@uxbert.com>
2020-10-25 22:24:54 +0200

    Updates

---
+++ precache-manifest.baf42b546d4ffb2cd4cb513290ec76cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7acb9c761d99a4ac65650c91ed5b588d",
+    "url": "/geowox/index.html"
+  },
+  {
+    "revision": "2857f3d35a121c43f992",
+    "url": "/geowox/static/css/main.5d64c98b.chunk.css"
+  },
+  {
+    "revision": "bfcb8d381721192ff5e3",
+    "url": "/geowox/static/js/2.f84d43a1.chunk.js"
+  },
+  {
+    "revision": "aa9407ae1ceec3e1c3e01851b31382e8",
+    "url": "/geowox/static/js/2.f84d43a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2857f3d35a121c43f992",
+    "url": "/geowox/static/js/main.cb50eacd.chunk.js"
+  },
+  {
+    "revision": "de245d72843636220d7c",
+    "url": "/geowox/static/js/runtime-main.fd1440e8.js"
+  }
+]);
